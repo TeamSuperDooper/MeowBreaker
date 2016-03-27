42eb2d40be756f7e1d197672f18c138e2a620117
Brian Porter <briansuperdooper@gmail.com>
2016-03-27 16:04:22 -0400

    Kity test, fix rotation, fix collision (sorta)
    1) Fixed collision kinda - it's playable. We set background pic back
    more, fixing it.
    2) Added kitty, and basic code
    3) Stopped kitty from rotating

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class Brick : MonoBehaviour {
+    
+    void Awake()
+    {
+    }
 
     //this runs whenever anything collides with the brick
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
         //check if the ball hit us?
-        if (collisionInfo.gameObject.name == "Ball")
+        if (collisionInfo.gameObject.name == "Ball" || collisionInfo.gameObject.name == "Kitty")
+        {
             //starts the process to destroy.
             StartCoroutine(DestroyBrick());
+        }
     }
 
     //this waits 1 frame then destroys the brick.
@@ -20,5 +26,21 @@ public class Brick : MonoBehaviour {
 
         //this destroys the brick
         Destroy(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        //print((this.transform.parent.childCount - 1) + " Bricks Left");
+        print((GameObject.Find("Brick Holder").transform.childCount - 1) + " Bricks left");
+    }
+
+    //Super kitty destroy all bricks in path
+    IEnumerator DestroyBrickKitty()
+    {
+        //yield return null;
+
+        //this destroys the brick
+        Destroy(gameObject);
+        yield return null;
     }
 }
---
+++ Assets/Scripts/Init.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    class Init : MonoBehaviour
+    {
+        void Start()
+        {
+            print(GameObject.Find("Brick Holder").transform.childCount);
+        }
+    }
+}
