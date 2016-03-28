6de4a7e702bb793d0de2f00b2520d8ed1773f2aa
Brian Porter <briansuperdooper@gmail.com>
2016-03-27 21:06:44 -0400

    Started Kitty powerup, and added kitty powerup FX
    1) Kitty powerup: disappears when hit, and makes particle come out
    2) Kity powerup particle FX are super cool man!

---
+++ Assets/Scripts/KittyPowerup.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KittyPowerup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    //this runs whenever anything collides with the brick
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        //check if the ball hit us?
+        if (collisionInfo.gameObject.name == "Ball" || collisionInfo.gameObject.name == "Kitty")
+        {
+            print("test");
+
+            GameObject particle = (GameObject)Instantiate(GameObject.Find("KittyPowerupParticles"), transform.position, Quaternion.identity);
+            particle.transform.position = transform.position;
+            Destroy(particle, 10);
+
+            //starts the process to destroy.
+            StartCoroutine(DestroyKittyPowerup());
+        }
+    }
+
+    IEnumerator DestroyKittyPowerup()
+    {
+
+        yield return null;
+
+        //this destroys the brick
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
